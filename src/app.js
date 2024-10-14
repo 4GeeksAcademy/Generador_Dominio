6d7fcf3e7a1391b071424e17f7d50290b381a415
Alejandro <alejandrolujan1999@outlook.es>
2024-10-14 18:59:54 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-/* eslint-disable */
+/* eslint-disable*/
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/squirtle_squad.jpg";
+import "./assets/img/pokeball.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  //Declaramos los arrays
+  let pronombre = ["el", "tu", "ese", "este"];
+  let sustantivo = ["coche", "sapo", "perro", "gato", "ordenador"];
+  let adjetivo = ["feo", "bonito", "gordo", "flaco", "grande", "pequeño"];
+  let dominio = [".com", ".es", ".net"];
+  let dominios = "";
+  // Bucle externo
+  for (let i = 0; i < pronombre.length; i++) {
+    // Primer bucle anidado
+    for (let j = 0; j < sustantivo.length; j++) {
+      // Segundo bucle anidado
+      for (let k = 0; k < adjetivo.length; k++) {
+        // Tercer bucle anidado
+        for (let l = 0; l < dominio.length; l++) {
+          // Imprimir la combinación de elementos
+          dominios += `<p>https://www.${pronombre[i] +
+            sustantivo[j] +
+            adjetivo[k] +
+            dominio[l]}</p>`;
+        }
+      }
+    }
+  }
+  document.querySelector("#genDominios").innerHTML = dominios;
 };
